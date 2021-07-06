a5ed1a8f377285a3d4c334bad23350ed2bb6af7f
Abraham Assariparambil <Abraham.Assariparambil@education.nsw.gov.au>
2021-07-06 17:25:54 +1000

    Setup the "Help" button

--- Millionaire Marathon/Menu.vb
+++ Millionaire Marathon/Menu.vb
@@ -5,11 +5,15 @@
         panel.TopLevel = False
         pnl.Controls.Add(panel)
         panel.Show()
+        pnl.BringToFront()
     End Sub
 
     Private Sub Play_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
-        pnl.BringToFront()
         SwitchPanel(FrmSetup)
+    End Sub
+
+    Private Sub Help_Click(sender As Object, e As EventArgs) Handles btnHelp.Click
+        SwitchPanel(FrmHelp)
     End Sub
 
 End Class
